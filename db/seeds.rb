f95ba4c0e83ecad556186669fcbcbfa76971f386
Christine Brendle <christinebrendle@gmail.com>
2020-11-12 16:55:49 +0100

    mister cocktail - except delete dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
+tequila = Ingredient.create(name: "tequila")
+vodka = Ingredient.create(name: "vodka")
+tonic_water = Ingredient.create(name: "tonic water")
+soda_water = Ingredient.create(name: "soda water")
+rhum = Ingredient.create(name: "rhum")
+tomato_juice = Ingredient.create(name: "tomato juice")
+
+
+vodka_tonic = Cocktail.create(name: "vodka tonic")
+bloody_mary = Cocktail.create(name: "bloody mary")
+mojito = Cocktail.create(name: "mojito")
+rhum_lemon = Cocktail.create(name: "rhum lemon")
+
+
+
+
+
